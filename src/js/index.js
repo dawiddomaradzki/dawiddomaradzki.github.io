0913b7a49ac68b03a7e7e9644e817b9884912610
Dawid Domaradzki <dawid-kun@wp.pl>
2021-02-02 21:35:27 +0100

    webpack personal site

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,43 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+const list = document.querySelector('.projects-list--js');
+
+
+fetch('https://api.github.com/users/dawiddomaradzki/repos?sort=updated')
+    .then(resp => resp.json())
+    .then(resp => {
+        const repos = resp;
+        for (const repo of repos) {
+            console.log(repo);
+            const { description, homepage, html_url, name } = repo;
+            list.innerHTML += `
+
+       <li class="project">
+            <div class="project__container">
+              <img src="src/assets/img/githubpurple.svg" alt="github photo">
+              <h3 class="project__title">${name}</h3>
+              ${
+                description ? `<p class="project__description">${description}</p>` : 'No description'
+                }
+              
+            </div>
+            <div class="project__footer">
+
+            ${
+                homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}">Demo</a>` : ''
+                }
+              
+              <a class="project__link project__link--source" href="${html_url}" target="_blank" rel="nofollow noreferrer"  title="Source code: ${name}">Github</a>
+            </div>
+          </li>
+      `;
+        }
+    })
+
+    .catch(err => {
+        console.log(err);
+    })
+
