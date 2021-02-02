cfd40725bbff44e84fb96fb3fd16c3742d15b614
Dawid Domaradzki <dawid-kun@wp.pl>
2021-02-02 23:53:49 +0100

    js description finding function

--- src/js/index.js
+++ src/js/index.js
@@ -19,27 +19,29 @@ fetch('https://api.github.com/users/dawiddomaradzki/repos?sort=updated')
         for (const repo of repos) {
             console.log(repo);
             const { description, homepage, html_url, name } = repo;
-            list.innerHTML += `
+            if (description) {
+                list.innerHTML += `
 
        <li class="project">
             <div class="project__container">
             <img src="../img/githubpurple.de42a557.svg" alt="github photo">
               <h3 class="project__title">${name}</h3>
               ${
-                description ? `<p class="project__description">${description}</p>` : 'No description'
-                }
+                    description ? `<p class="project__description">${description}</p>` : 'No description'
+                    }
               
             </div>
             <div class="project__footer">
 
             ${
-                homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}">Demo</a>` : ''
-                }
+                    homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}">Demo</a>` : ''
+                    }
               
               <a class="project__link project__link--source" href="${html_url}" target="_blank" rel="nofollow noreferrer"  title="Source code: ${name}">Github</a>
             </div>
           </li>
       `;
+            }
         }
     })
 
